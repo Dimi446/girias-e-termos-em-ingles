46996ea697d5dc0b8209d1aa1c763c31cd85b52f
Dimitri T. Pereira <nsdk291@gmail.com>
2024-11-13 14:30:04 -0300

    adicionei os arquivo

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Gíria 1',
+    'O que é mewing?',
+    'O mewing é a técnica de achatar a língua contra o céu da boca. Com o tempo, se diz que o movimento ajuda a realinhar os dentes e a definir a linha da mandíbula.'
+)
+
+criaCartao(
+    'Gíria 2',
+    'Qual o significado de "fr"?',
+    'O termo “FR” é uma abreviação de “For Real” ou “For Real? ” e é frequentemente usado para expressar se o que alguém está dizendo é genuíno, semelhante a dizer “sério” ou “verdadeiro”.'
+)
+
+criaCartao(
+    'Gíria 3',
+    'O que quer dizer btw?',
+    '"BTW" é uma encurtação da expressão "by the way", que significa "por falar nisso" ou "a propósito".'
+)
+
+criaCartao(
+    'Gíria 4',
+    'O que significa "ASAP"?',
+    'ASAP é uma expressão curta para "as soon as possible", que quer dizer "o mais rápido/breve possível"'
+)
