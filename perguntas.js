0bdf2b8f1d208d774170fd502b49d03096cd32c8
Dimitri T. Pereira <nsdk291@gmail.com>
2024-11-29 14:20:45 -0300

    adicionei mais algumas perguntas

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,27 @@ criaCartao(
     'O que significa "ASAP"?',
     'ASAP é uma expressão curta para "as soon as possible", que quer dizer "o mais rápido/breve possível"'
 )
+
+criaCartao(
+    'Gíria 5',
+    'O que significa "chill out"?',
+    'Usada para pedir que alguém se acalme ou relaxe, em situações onde a pessoa está nervosa ou estressada.'
+)
+
+criaCartao(
+    'Gíria 6',
+    'O que é "dope"?',
+    'Usada para descrever algo que é muito legal, impressionante ou de alta qualidade.'
+)
+
+criaCartao(
+    'Gíria 7',
+    'O que quer dizer "GOAT"?',
+    'Um acrônimo para "Greatest Of All Time" (O Maior de Todos os Tempos). É usado para descrever alguém que é o melhor em sua área.'
+)
+
+criaCartao(
+    'Gíria 8',
+    'O que seria "slay"?',
+    'Usada para descrever alguém que fez algo de maneira excelente ou que está muito bem vestido.'
+)
